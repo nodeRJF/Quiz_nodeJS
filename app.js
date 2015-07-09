b0760ae717e3cb5729e015579919a427cc199c61
nodeRJF <jimenor@gmail.com>
2015-07-09 21:35:55 +0200

    correccion de errata

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var partials = require('express-partialS');
+var partials = require('express-partials');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
