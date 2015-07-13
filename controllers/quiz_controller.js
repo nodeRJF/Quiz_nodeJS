144bf4afe70dfae1cbe833d6a468bf05312fb5e0
nodeRJF <jimenor@gmail.com>
2015-07-13 20:48:22 +0200

    primera conexion BBDD
    se utiliza sqlite y sequelize

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,5 @@
 
-
+/*
 exports.question=function(req, res) {
   console.log("enroutador get QUESTION");
   res.render('quizes/question', { pregunta: 'Capital de Italia' });
@@ -14,4 +14,24 @@ exports.answer=function(req, res) {
 			mensaje=  "Lo siento '"+ req.query.respuesta +"' es incorrecta."
 	}
   res.render('quizes/answer', { respuesta: mensaje});
+};
+*/
+
+var models =  require('../models/models.js');
+exports.question=function(req, res){
+  models.Quiz.findAll().success(function(quiz){
+    res.render('quizes/question',{pregunta:quiz[0].pregunta});
+  });
+};
+
+exports.answer=function(req, res){
+
+  models.Quiz.findAll().success(function(quiz){
+    //console.log("."+req.query.respuesta+"respuesta:"+quiz[0].respuesta+".");
+    if(req.query.respuesta === quiz[0].respuesta ){
+      res.render('quizes/answer',{respuesta:'Correcto'});
+    } else{
+      res.render('quizes/answer',{respuesta:'Incorrecto'});
+    }
+  });
 };
---
+++ models/models.js
@@ -0,0 +1,20 @@
+var path=require('path');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(null, null, null,
+                              {dialect:'sqlite', store:'quiz.sqlite'}
+                            );
+
+var Quiz =  sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz = Quiz;
+
+sequelize.sync().success(function(){
+  Quiz.count().success(function(count){
+    if(count === 0){
+      Quiz.create({pregunta: 'Capital de Italia',
+                   respuesta: 'Roma'
+                 })
+      .success(function(){console.log('BBDD inicializada.');});
+    };
+  });
+});
